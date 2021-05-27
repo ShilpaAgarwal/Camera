82a48d8751286c5f7909bfe33ff0be3df90e85fa
Shilpa <agarwalshilpa11012001@gmail.com>
2021-05-27 13:19:26 +0530

    Added filters and picture click feature

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let videoElem = document.querySelector("video");
 // let audioElem = document.querySelector("audio");
 let recordBtn = document.querySelector("button");
+let picBtn = document.querySelector(".click");
+let filterLayer = document.querySelector(".filter-layer");
+let filterInner = document.querySelectorAll(".filter-inner");
+let timing = document.querySelector(".timing");
+let filterColor = "";
+let counter = 0;
+let clearObj;
 let constraint = {
     audio: true,
     video: true,
@@ -10,6 +17,7 @@ let recording = [];
 let isRecording = false;
 let isPaused = false;
 let mediaRecordingObjectForCurrStream;
+
 // gives a promise that asks browser to gives the access to media devices,
 // which are given as input in constraint
 let userMediaPromise = navigator.mediaDevices.getUserMedia(constraint);
@@ -17,12 +25,13 @@ userMediaPromise
     .then(function (stream) {
         // browser stream is given to audio and video
         videoElem.srcObject = stream;
+
         // audioElem.srcObject = stream;
         // after this recording can be enabled
         mediaRecordingObjectForCurrStream = new MediaRecorder(stream);
         // camera recording add -> recording array
-        console.log(mediaRecordingObjectForCurrStream);
         mediaRecordingObjectForCurrStream.ondataavailable = function (e) {
+            
             recording.push(e.data);
         };
         //download
@@ -41,8 +50,10 @@ userMediaPromise
                 a.href = url;
                 a.click();
                 recording = [];
+                
             }
         );
+        
     })
     .catch(function (err) {
         alert("Please give access to audio and video ");
@@ -57,11 +68,65 @@ recordBtn.addEventListener("click", function () {
     if (isRecording == false) {
         mediaRecordingObjectForCurrStream.start();
         recordBtn.innerText = "Recording...";
+        startTimer();
     }
     // is recording is started, stop it
     else {
         mediaRecordingObjectForCurrStream.stop();
         recordBtn.innerText = "Record";
+        stopTimer();
     }
     isRecording = !isRecording;
 });
+picBtn.addEventListener("click", function (e) {
+    // create canvas
+    let canvas = document.createElement("canvas");
+    canvas.height = videoElem.videoHeight;
+    canvas.width = videoElem.videoWidth;
+    console.log(videoElem.videoWidth);
+    let tool = canvas.getContext("2d");
+    tool.drawImage(videoElem, 0, 0);
+    
+    if(filterColor) {
+        tool.fillStyle = filterColor;
+        tool.fillRect(0, 0, canvas.width, canvas.height)
+    }
+    // download the imahe
+    let a = document.createElement("a");
+    let url = canvas.toDataURL();
+    a.download = "file.png";
+    a.href = url;
+    a.click();
+    a.remove();
+})
+
+for(let i=0; i<filterInner.length; i++) {
+    filterInner[i].addEventListener("click", function() {
+        filterColor = filterInner[i].style.backgroundColor;
+        filterLayer.style.backgroundColor = filterColor; 
+        // filterLayer.style.width = videoElem.videoWidth;
+        // console.log(videoElem.videoWidth);
+        // console.log(filterLayer.style.width);
+    })
+}
+
+function startTimer() {
+    timing.style.display = "block";
+    function fn() {
+        // hours
+        let hours = Number.parseInt(counter/3600);
+        hours = hours<10? `0${hours}`:hours;
+        let min = Number.parseInt(counter%3600/60);
+        min = min<10? `0${min}`:min;
+        let second = Number.parseInt(counter%60);
+        second = second<10? `0${second}`:second;
+        timing.innerText = `${hours}:${min}:${second}`;
+        counter++;
+    }
+    clearObj = setInterval(fn, 1000);
+
+}
+function stopTimer() {
+    timing.style.display = "none";
+    clearInterval(clearObj)
+}
